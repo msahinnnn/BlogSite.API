c71db764ec1503da9520377c8a97cda192b38018
msahinnnn <mehmet.sahin9910@gmail.com>
2023-06-09 12:22:25 +0300

    CommentCacheService unit tests.

--- BlogSite.Business.Test/CommentCacheServiceTest.cs
+++ BlogSite.Business.Test/CommentCacheServiceTest.cs
@@ -12,6 +12,53 @@ namespace BlogSite.Business.Test
 {
     public class CommentCacheServiceTest
     {
-        
+        public ICommentRedisService _redisService;
+        public ICommentCacheService _commentCacheService;
+
+        public CommentCacheServiceTest()
+        {
+            _redisService = A.Fake<ICommentRedisService>();
+            _commentCacheService = new CommentCacheService(_redisService);
+        }
+
+        [Fact]
+        public async void GetComments_ReturnsCommentsFromCache()
+        {
+            var comments = A.Fake<List<Comment>>();
+            var x = A.CallTo(() => _redisService.GetAsync()).Returns(comments);
+
+            var result = await _commentCacheService.GetAsync();
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async void GetCommentsById_ReturnsCommentFromCache()
+        {
+            var comment = A.Fake<Comment>();
+            A.CallTo(() => _redisService.GetByIdAsync(comment.Id)).Returns(comment);
+
+            var result = await _commentCacheService.GetByIdAsync(comment.Id);
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async void DeleteComments_ReturnsTrue()
+        {
+            var comment = A.Fake<Comment>();
+            var x = A.CallTo(() => _redisService.DeleteAsync(comment.Id)).Returns(true);
+
+            var result = await _commentCacheService.DeleteAsync(comment.Id);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async void SaveOrUpdateComment_ReturnsTrue()
+        {
+            var comment = A.Fake<Comment>();
+            A.CallTo(() => _redisService.SaveOrUpdateAsync(comment)).Returns(true);
+
+            var result = await _commentCacheService.SaveOrUpdateAsync(comment);
+            Assert.True(result);
+        }
     }
 }
